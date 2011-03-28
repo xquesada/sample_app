0debc0205ad0a02e62d08251f3a1104cbee1a9cb
Xavier Quesada Allue <xavier@xqa.com.ar>
2011-03-28 17:45:29 +0200

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
   get "pages/about"
   get "pages/help"
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+    
 end
